0c29c0d5de5662fabaa320821fe60bed8f8a8d6d
SuirlCore <152679258+SuirlCore@users.noreply.github.com>
2025-02-12 13:56:40 -0700

    update

---
+++ upload.php
@@ -0,0 +1,12 @@
+<?php
+if (!empty($_FILES['file'])) {
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["file"]["name"]);
+    
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+        echo "File uploaded successfully!";
+    } else {
+        echo "Error uploading file.";
+    }
+}
+?>
